2c03f426fc72e88f9592a0367d8057b51a9d5161
Dhiraj Maan <dhirajmaanshah@gmail.com>
2021-09-13 23:34:30 +0530

    deleated commented code

--- lib/models/dish.dart
+++ lib/models/dish.dart
@@ -23,18 +23,3 @@ List<Dish> addOn = [
   Dish(id: 9, path: "images/add-on/Salad.png"),
   Dish(id: 10, path: "images/add-on/Soup.png"),
 ];
-
-// const List<Map<int, dynamic>> dish = <Map<int, dynamic>>[
-//   <int, dynamic>{1: "images/meals/Sabji_Roti.png"},
-//   <int, dynamic>{2: "images/meals/Dal_Roti.png"},
-//   <int, dynamic>{3: "images/meals/Curry_Rice.png"},
-//   <int, dynamic>{4: "images/meals/Dal_Rice.png"},
-//   <int, dynamic>{5: "images/meals/Sabji_Dal_Rice.png"},
-//   <int, dynamic>{6: "images/meals/Sabji_Dal_Roti_Rice.png"},
-//   <int, dynamic>{8: "images/meals/Curry_Roti_Rice.png"},
-//   <int, dynamic>{7: "iimages/meals/Sabji_Dal_Roti.png"},
-// ];
-// const List<Map<int, dynamic>> addOn = <Map<int, dynamic>>[
-//   <int, dynamic>{9: "images/add-on/Salad.png"},
-//   <int, dynamic>{10: "images/add-on/Soup.png"},
-// ];
